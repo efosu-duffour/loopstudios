59af9d2caaf11d4e52d91adbc439f66f34e2bac0
efosu-duffour <emman.duffour@gmail.com>
2025-03-10 20:24:43 +0000

    Fix: Modified img 'src' to show images

--- src/main.ts
+++ src/main.ts
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         if (Math.floor(header_cont.contentRect.width) > 667) {
             if (!mobileNav.classList.contains("close")) {
-                (menuButton.querySelector("img") as HTMLImageElement).src = "/images/icon-hamburger.svg";
+                (menuButton.querySelector("img") as HTMLImageElement).src = "/loopstudios/images/icon-hamburger.svg";
                 animateMenuOut();
                 scrollBody(true);
                 mobileNav.classList.add("close");
@@ -44,12 +44,12 @@ document.addEventListener("DOMContentLoaded", () => {
         mobileNav.classList.toggle("close");
 
         if (mobileNav.classList.contains("close")) {
-            (menuButton.querySelector("img") as HTMLImageElement).src = "/images/icon-hamburger.svg";
+            (menuButton.querySelector("img") as HTMLImageElement).src = "loopstudios/images/icon-hamburger.svg";
             scrollBody(true);
             animateMenuOut();
         }
         else {
-            (menuButton.querySelector("img") as HTMLImageElement).src = "/images/icon-close.svg";
+            (menuButton.querySelector("img") as HTMLImageElement).src = "loopstudios/images/icon-close.svg";
             scrollBody(false);
             animateMenuIn();
         }
